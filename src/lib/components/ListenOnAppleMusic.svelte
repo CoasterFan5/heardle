e6b540b892debf3a6c8e682af768e87d9bbe7028
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2026-03-12 14:25:19 -0400

    song card and better detection

---
+++ src/lib/components/ListenOnAppleMusic.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { SongObject } from '$lib/types/song';
+
+	const {
+		song
+	}: {
+		song: SongObject;
+	} = $props();
+</script>
+
+<a href="https://music.apple.com/us/song/{song.id}" target="_blank"
+	><img src="/listenOnAppleMusic.svg" alt="Listen on Apple Music" /></a
+>
+
+<style lang="scss">
+	a {
+		padding: 0.5rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
---
+++ src/lib/components/SongCard.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import type { SongObject } from '$lib/types/song';
+	import AppleMusicIcon from './AppleMusicIcon.svelte';
+
+	const {
+		song
+	}: {
+		song: SongObject;
+	} = $props();
+</script>
+
+<div class="songCard">
+	<div class="image">
+		<AppleMusicIcon url={song.attributes.artwork.url} />
+	</div>
+	<div class="text">
+		<span class="songName">
+			{song.attributes.name}
+		</span>
+		<span class="artist">
+			{song.attributes.artistName}
+		</span>
+	</div>
+</div>
+
+<style lang="scss">
+	.songCard {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: start;
+		gap: 0.25rem;
+	}
+
+	.text {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.artist {
+		font-size: 0.8rem;
+		opacity: 0.8;
+	}
+</style>

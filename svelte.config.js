9891a74e1fc6d78cee9b783ec29f1623f8116396
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2026-03-12 14:28:16 -0400

    change adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,15 +7,15 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		experimental: {
-					remoteFunctions: true
+			remoteFunctions: true
 		},
 		adapter: adapter()
 	},
 	compilerOptions: {
-			experimental: {
-				async: true
-			}
-	},
+		experimental: {
+			async: true
+		}
+	}
 };
 
 export default config;
